c662b680f971a77358fe8ffb744182501802121c
alcoholisme <212327835+alcoholisme@users.noreply.github.com>
2026-04-14 09:51:39 +0530

    Jenkins-pipeline

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,18 +1,21 @@
-package com.example;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-public class App {
+public class Main {
     public static void main(String[] args) {
 
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.saucedemo.com/");
-        driver.manage().window().maximize();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new"); // critical
+        options.addArguments("--no-sandbox");   // critical for Jenkins
+        options.addArguments("--disable-dev-shm-usage"); // prevents crashes
+        options.addArguments("--disable-gpu");
 
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://www.google.com");
+        System.out.println(driver.getTitle());
+
+        driver.quit();
     }
 }
