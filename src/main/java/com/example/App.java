80fa4a15bf60e3d525be4470253f24a7c52888c4
alcoholisme <212327835+alcoholisme@users.noreply.github.com>
2026-04-14 09:55:37 +0530

    jenkins-pipeline

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,20 +1,27 @@
+package com.example;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class Main {
+public class App {
     public static void main(String[] args) {
 
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new"); // critical
-        options.addArguments("--no-sandbox");   // critical for Jenkins
-        options.addArguments("--disable-dev-shm-usage"); // prevents crashes
-        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--headless=new");   // 
 
         WebDriver driver = new ChromeDriver(options);
 
-        driver.get("https://www.google.com");
+        driver.get("https://www.saucedemo.com/");
         System.out.println(driver.getTitle());
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
 
         driver.quit();
     }
